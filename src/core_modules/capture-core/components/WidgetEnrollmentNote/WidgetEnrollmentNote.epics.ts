f933fca227b544e29b4e8672fdf02395f494dfa7
henrikmv <henrik@devOtta.no>
2026-05-28 17:13:56 +0200

    feat: implement remove enrollment note action and epic

--- src/core_modules/capture-core/components/WidgetEnrollmentNote/WidgetEnrollmentNote.epics.ts
+++ src/core_modules/capture-core/components/WidgetEnrollmentNote/WidgetEnrollmentNote.epics.ts
@@ -6,13 +6,17 @@ import uuid from 'd2-utilizr/lib/uuid';
 import moment from 'moment';
 import type { ReduxStore, ApiUtils, EpicAction } from 'capture-core-utils/types/global';
 import { CurrentUser } from '../../utils/userInfo/CurrentUser';
-import { actionTypes, batchActionTypes, startAddNoteForEnrollment, addEnrollmentNote }
+import { actionTypes, batchActionTypes, startAddNoteForEnrollment, addEnrollmentNote, removeEnrollmentNote }
     from './WidgetEnrollmentNote.actions';
 import type { ClientNote, SaveContext } from './WidgetEnrollmentNote.types';
 
 type AddNoteActionPayload = {
     enrollmentId: string;
     note: string;
+};
+
+type RemoveNoteActionMeta = {
+    context: SaveContext;
 };
 
 const createServerData = (note: string, useNewEndpoint: boolean): Record<string, unknown> => {
@@ -58,4 +62,14 @@ export const addNoteForEnrollmentEpic = (
                 startAddNoteForEnrollment(enrollmentId, serverData, state.currentSelections, saveContext),
                 addEnrollmentNote(enrollmentId, clientNote),
             ], batchActionTypes.ADD_NOTE_BATCH_FOR_ENROLLMENT);
+        }));
+
+export const removeNoteForEnrollmentEpic = (action$: EpicAction<any, RemoveNoteActionMeta>) =>
+    action$.pipe(
+        ofType(actionTypes.ADD_NOTE_FAILED_FOR_ENROLLMENT),
+        map((action: { meta: { context: SaveContext } }) => {
+            const { enrollmentId, noteClientId } = action.meta.context;
+            return batchActions([
+                removeEnrollmentNote(enrollmentId, noteClientId),
+            ], batchActionTypes.REMOVE_NOTE_BATCH_FOR_ENROLLMENT);
         }));
